2f5a9f1562556037a39710a9a4a7d448f77f74eb
cdc-dpbrown <18171996+cdc-dpbrown@users.noreply.github.com>
2019-03-13 09:50:30 -0400

    Added build via MSBuild and quiet mode inital check-in

--- epi-info-scorched-earth-build.bat
+++ epi-info-scorched-earth-build.bat
@@ -1,6 +1,35 @@
+::CLS
 @ECHO OFF
 COLOR
 
+SET EI_KEY_QUIET=Q
+
+SET EI_ARGS=%1
+ECHO EI_ARGS=%EI_ARGS%
+ECHO EI_KEY_QUIET=%EI_KEY_QUIET%
+SET EI_QUIET=TRUE
+
+setLocal EnableDelayedExpansion
+
+IF NOT "x!EI_ARGS:%EI_KEY_QUIET%=!"=="x%EI_ARGS%" (
+    SET EI_QUIET=TRUE
+    ECHO TR
+) ELSE (
+    SET EI_QUIET=FALSE
+    ECHO FS
+)
+
+endlocal
+
+ECHO EI_QUIET=%EI_QUIET%
+ECHO EI_ARGS=%EI_ARGS%
+
+::GOTO:EOF
+
+:: ===============================================================
+:: DELETE EPI INFO FOLDER
+:: ===============================================================
+IF %EI_QUIET%==TRUE GOTO:DELETEEPIINFOFOLDER
 SET /P d=DELETE EPI INFO FOLDER [Y/N]?
 IF /I "%d%" EQU "Y" GOTO :DELETEEPIINFOFOLDER
 IF /I "%d%" EQU "N" GOTO :SKIP_DELETE
@@ -20,16 +49,20 @@ IF EXIST ".\Epi-Info-Community-Edition" (
 IF EXIST ".\Epi-Info-Community-Edition" (
     COLOR 0A
     ECHO Epi-Info-Community-Edition - still there - try restartExplorer.bat
-    PAUSE
+    EXIT /B
 ) ELSE (
     COLOR
     ECHO Epi-Info-Community-Edition - gone
 )
 
 :SKIP_DELETE
+:: ===============================================================
 
-COLOR 
 
+:: ===============================================================
+:: GET SOURCE - GIT CLONE EPI INFO
+:: ===============================================================
+IF %EI_QUIET%==TRUE GOTO:GET_SOURCE
 SET /P o=OVERWRITE (GET) EPI INFO FROM GITHUB [Y/N]?
 IF /I "%o%" EQU "Y" GOTO :GET_SOURCE
 IF /I "%o%" EQU "N" GOTO :SKIP_GET_SOURCE
@@ -43,18 +76,17 @@ git clone https://github.com/Epi-Info/Epi-Info-Community-Edition.git
 CD Epi-Info-Community-Edition
 
 COLOR 
-PAUSE
+IF NOT %EI_QUIET%==TRUE PAUSE
 REM [COMMENT] Dashboard: Added paired t-test results to HTML output
 REM LAST CHECK-IN BEFORE CHANGE TO .NET 4.6.1 
 ::git reset --hard b414fa8dac477e93c1c0dad2b4080425044657d5
 
 REM [COMMENT] [BUILD] 7.2.2.16 11/2/2018
-git reset --hard 3d7b050cbde62137f77d9ffc4175a0ae8c409935
+::git reset --hard 3d7b050cbde62137f77d9ffc4175a0ae8c409935
 
+:: ===============================================================
 :SKIP_GET_SOURCE
-
-:UPDATE_VERSION_INFO
-::CD Epi-Info-Community-Edition
+:: ===============================================================
 
 :: ===============================================================
 :: UPDATE VERSION
@@ -74,28 +106,36 @@ git reset --hard 3d7b050cbde62137f77d9ffc4175a0ae8c409935
 :: AssemblyInfo.vb
 :: <Assembly: AssemblyVersion("7.2.2.16")>
 :: <Assembly: AssemblyFileVersion("7.2.2.16")>
-
+IF %EI_QUIET%==TRUE GOTO:SKIP_UPDATE_VERSION
 CALL code -n SolutionInfo.cs .\EpiInfoPlugin\Properties\AssemblyInfo.cs ".\StatisticsRepository\My Project\AssemblyInfo.vb"
-PAUSE
+:SKIP_UPDATE_VERSION
+:: ===============================================================
+
 
 :: ===============================================================
-:: CHANGE TO RELEASE KEYS
+:: COPY KEYS
 :: ===============================================================
+IF %EI_QUIET%==TRUE GOTO:SKIP_KEYS_COPY
+:: ---------------------------------------------------------------
+:: CHANGE TO RELEASE KEYS
+:: ---------------------------------------------------------------
 :: GET RELEASE KEY VERSION OF ConfigurationStatic.cs
 COPY /Y "C:\requiredFiles (ei7)\Configuration_Static.cs" .\Epi.Core\Configuration_Static.cs
 ::ECHO OPEN IN CODE TO VERIFY ONLY KEYS HAVE CHANGED
 ::CALL code -n .\Epi.Core\Configuration_Static.cs
 ::PAUSE
-
-:: ===============================================================
+:: ---------------------------------------------------------------
 :: REPLACE THE COMPONENT ART LICENSE
-:: ===============================================================
+:: ---------------------------------------------------------------
 COPY /Y "C:\requiredFiles (ei7)\ComponentArt.Win.DataVisualization.lic" .\Epi.Windows.AnalysisDashboard\ComponentArt.Win.DataVisualization.lic
 COPY /Y "C:\requiredFiles (ei7)\ComponentArt.Win.DataVisualization.lic" .\Epi.Windows.Enter\ComponentArt.Win.DataVisualization.lic
 COPY /Y "C:\requiredFiles (ei7)\ComponentArt.Win.DataVisualization.lic" .\EpiDashboard\ComponentArt.Win.DataVisualization.lic
 ::ECHO OPEN IN CODE TO VERIFY THE COMPONENT LICENSE HAS CHANGED
 CALL code -n .
 PAUSE
+:SKIP_KEYS_COPY
+:: ===============================================================
+
 
 :: ===============================================================
 :: COPY DLLS
@@ -118,18 +158,10 @@ COPY /Y "C:\requiredFiles (ei7)\dll\Mono.Security.dll" .\build\release\Mono.Secu
 COPY /Y "C:\requiredFiles (ei7)\dll\Npgsql.dll" .\build\release\Npgsql.dll
 
 :: ===============================================================
-:: COPY LAUNCH EPI INFO EXECUTABLE 
-:: ===============================================================
-COPY /Y "C:\requiredFiles (ei7)\Launch Epi Info 7.exe" ".\build\release\Launch Epi Info 7.exe"
-
-
-:: ===============================================================
 :: COPY PROJECT (RELEASE) DIRECTORY
 :: ===============================================================
 
-
-ECHO %CD%
-PAUSE
+IF NOT %EI_QUIET%==TRUE PAUSE
 
 :: ===============================================================
 ::
@@ -145,11 +177,21 @@ PAUSE
 :: devenv mysln.sln /build Release /project proj1 /projectconfig Release
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" "Epi Info 7.sln" /p:Configuration=Release /p:Platform=x86
-PAUSE
+IF NOT %EI_QUIET%==TRUE PAUSE
 
+:: ===============================================================
+:: COPY LAUNCH EPI INFO EXECUTABLE 
+:: ===============================================================
+@ECHO ON
+COPY /Y "C:\requiredFiles (ei7)\Launch Epi Info 7.exe" ".\build\release\Launch Epi Info 7.exe"
+@ECHO OFF
+
+
+@ECHO ON
 git checkout -- *
 git status
-PAUSE
+IF NOT %EI_QUIET%==TRUE PAUSE
+@ECHO OFF
 
 :: ===============================================================
 :: DELETE THEN COPY PROJECT (RELEASE) DIRECTORY
@@ -160,22 +202,15 @@ PAUSE
 :: PRUNE FILES
 :: ===============================================================
 
-DEL /Q .\package-lock.json
-
-
-IF EXIST ".\package-lock.json" (
-    ECHO.
-    ECHO Deleting package-lock.json
-    DEL /Q .\package-lock.json
-)
 
 :: ===============================================================
 :: RENAME AND ZIP
 :: ===============================================================
 
 
-:: explorer .
+::explorer ".\build\release\"
+".\build\release\Menu.exe"
 
 
 
-
+:EOF
