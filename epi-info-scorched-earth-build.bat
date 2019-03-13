5b1de38e9d1f9cf078093beeed2812ddec54e0ab
cdc-dpbrown <18171996+cdc-dpbrown@users.noreply.github.com>
2019-03-13 16:30:32 -0400

    Fixed issue with QUIET and added HELP(/?)

--- epi-info-scorched-earth-build.bat
+++ epi-info-scorched-earth-build.bat
@@ -2,23 +2,29 @@ CLS
 @ECHO OFF
 COLOR
 
-setLocal EnableDelayedExpansion
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-SET EI_KEY_QUIET=Q
+SET KEY_QUIET=Q
+SET KEY_HELP=/?
 
-SET EI_ARGS=%1
-ECHO EI_ARGS=%EI_ARGS%
-ECHO EI_KEY_QUIET=%EI_KEY_QUIET%
-SET EI_QUIET=FALSE
+SET ARGS=%1
+IF "%ARGS%" EQU "" SET ARGS=1
+::ECHO ARGS=%ARGS%
+::ECHO KEY_QUIET=%KEY_QUIET%
+SET QUIET=FALSE
+SET HELP=FALSE
 
-IF NOT "x!EI_ARGS:%EI_KEY_QUIET%=!"=="x%EI_ARGS%" SET EI_QUIET=TRUE
-::ECHO QUIET = %EI_QUIET%
-::ECHO ARGS = %EI_ARGS%
+IF NOT "x!ARGS:%KEY_QUIET%=!"=="x%ARGS%" SET QUIET=TRUE
+IF NOT "x!ARGS:%KEY_HELP%=!"=="x%ARGS%" GOTO :HELP
+::ECHO QUIET: %QUIET%
+::ECHO ARGS: %ARGS%
+
+::GOTO:EOF
 
 :: ===============================================================
 :: DELETE EPI INFO FOLDER
 :: ===============================================================
-IF %EI_QUIET%==TRUE GOTO:DELETEEPIINFOFOLDER
+IF % QUIET%==TRUE GOTO:DELETEEPIINFOFOLDER
 SET /P d=DELETE EPI INFO FOLDER [Y/N]?
 IF /I "%d%" EQU "Y" GOTO :DELETEEPIINFOFOLDER
 IF /I "%d%" EQU "N" GOTO :SKIP_DELETE
@@ -51,7 +57,7 @@ IF EXIST ".\Epi-Info-Community-Edition" (
 :: ===============================================================
 :: GET SOURCE - GIT CLONE EPI INFO
 :: ===============================================================
-IF %EI_QUIET%==TRUE GOTO:GET_SOURCE
+IF % QUIET%==TRUE GOTO:GET_SOURCE
 SET /P o=OVERWRITE (GET) EPI INFO FROM GITHUB [Y/N]?
 IF /I "%o%" EQU "Y" GOTO :GET_SOURCE
 IF /I "%o%" EQU "N" GOTO :SKIP_GET_SOURCE
@@ -65,7 +71,7 @@ git clone https://github.com/Epi-Info/Epi-Info-Community-Edition.git
 CD Epi-Info-Community-Edition
 
 COLOR 
-IF NOT %EI_QUIET%==TRUE PAUSE
+IF NOT % QUIET%==TRUE PAUSE
 REM [COMMENT] Dashboard: Added paired t-test results to HTML output
 REM LAST CHECK-IN BEFORE CHANGE TO .NET 4.6.1 
 ::git reset --hard b414fa8dac477e93c1c0dad2b4080425044657d5
@@ -95,7 +101,7 @@ REM [COMMENT] [BUILD] 7.2.2.16 11/2/2018
 :: AssemblyInfo.vb
 :: <Assembly: AssemblyVersion("7.2.2.16")>
 :: <Assembly: AssemblyFileVersion("7.2.2.16")>
-IF %EI_QUIET%==TRUE GOTO:SKIP_UPDATE_VERSION
+IF % QUIET%==TRUE GOTO:SKIP_UPDATE_VERSION
 CALL code -n SolutionInfo.cs .\EpiInfoPlugin\Properties\AssemblyInfo.cs ".\StatisticsRepository\My Project\AssemblyInfo.vb"
 :SKIP_UPDATE_VERSION
 :: ===============================================================
@@ -104,7 +110,7 @@ CALL code -n SolutionInfo.cs .\EpiInfoPlugin\Properties\AssemblyInfo.cs ".\Stati
 :: ===============================================================
 :: COPY KEYS
 :: ===============================================================
-IF %EI_QUIET%==TRUE GOTO:SKIP_KEYS_COPY
+IF % QUIET%==TRUE GOTO:SKIP_KEYS_COPY
 :: ---------------------------------------------------------------
 :: CHANGE TO RELEASE KEYS
 :: ---------------------------------------------------------------
@@ -150,7 +156,7 @@ COPY /Y "C:\requiredFiles (ei7)\dll\Npgsql.dll" .\build\release\Npgsql.dll
 :: COPY PROJECT (RELEASE) DIRECTORY
 :: ===============================================================
 
-IF NOT %EI_QUIET%==TRUE PAUSE
+IF NOT % QUIET%==TRUE PAUSE
 
 :: ===============================================================
 ::
@@ -166,7 +172,7 @@ IF NOT %EI_QUIET%==TRUE PAUSE
 :: devenv mysln.sln /build Release /project proj1 /projectconfig Release
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" "Epi Info 7.sln" /p:Configuration=Release /p:Platform=x86
-IF NOT %EI_QUIET%==TRUE PAUSE
+IF NOT % QUIET%==TRUE PAUSE
 
 :: ===============================================================
 :: COPY LAUNCH EPI INFO EXECUTABLE 
@@ -179,7 +185,7 @@ COPY /Y "C:\requiredFiles (ei7)\Launch Epi Info 7.exe" ".\build\release\Launch E
 @ECHO ON
 git checkout -- *
 git status
-IF NOT %EI_QUIET%==TRUE PAUSE
+IF NOT % QUIET%==TRUE PAUSE
 @ECHO OFF
 
 :: ===============================================================
@@ -198,9 +204,14 @@ IF NOT %EI_QUIET%==TRUE PAUSE
 
 
 ::explorer ".\build\release\"
-".\build\release\Menu.exe"
+CALL ".\build\release\Menu.exe"
 
 
-endlocal
+ENDLOCAL
+
+:HELP
+ECHO Q   Quiet mode
+EXIT /B
 
 :EOF
+EXIT /B
