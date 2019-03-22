f69060947092f97800216b9851a44cf51099fe0b
cdc-dpbrown <18171996+cdc-dpbrown@users.noreply.github.com>
2019-03-22 09:27:56 -0400

    Progress with Epi Info 7 builds

--- epi-info-scorched-earth-build.bat
+++ epi-info-scorched-earth-build.bat
@@ -1,6 +1,6 @@
 CLS
 @ECHO OFF
-COLOR
+COLOR 0E
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
@@ -20,16 +20,16 @@ IF NOT "x!ARGS:%KEY_HELP%=!"=="x%ARGS%" GOTO :HELP
 ::ECHO ARGS: %ARGS%
 
 ::GOTO:EOF
-
-:: ===============================================================
-:: DELETE EPI INFO FOLDER
-:: ===============================================================
+ECHO :: ===============================================================
+ECHO :: DELETE EPI INFO FOLDER
+ECHO :: ===============================================================
 IF %QUIET%==TRUE GOTO :DELETEEPIINFOFOLDER
+:ASK_SKIP_DELETE
 SET /P d=DELETE EPI INFO FOLDER [Y/N]?
 IF /I "%d%" EQU "Y" GOTO :DELETEEPIINFOFOLDER
 IF /I "%d%" EQU "N" GOTO :SKIP_DELETE
+GOTO :ASK_SKIP_DELETE
 GOTO :SKIP_DELETE
-
 :DELETEEPIINFOFOLDER
 
 IF EXIST ".\Epi-Info-Community-Edition" (
@@ -52,26 +52,28 @@ IF EXIST ".\Epi-Info-Community-Edition" (
 
 :SKIP_DELETE
 :: ===============================================================
+:: END OF DELETE EPI INFO FOLDER
+:: ===============================================================
 
 
-:: ===============================================================
-:: GET SOURCE - GIT CLONE EPI INFO
-:: ===============================================================
+ECHO :: ===============================================================
+ECHO :: GET SOURCE - GIT CLONE EPI INFO REPO
+ECHO :: ===============================================================
 IF %QUIET%==TRUE GOTO:GET_SOURCE
+:ASK_GET_SOURCE
 SET /P o=OVERWRITE (GET) EPI INFO FROM GITHUB [Y/N]?
 IF /I "%o%" EQU "Y" GOTO :GET_SOURCE
 IF /I "%o%" EQU "N" GOTO :SKIP_GET_SOURCE
-:SKIP_GET_SOURCE
-
+GOTO :ASK_GET_SOURCE
+GOTO :SKIP_GET_SOURCE
 :GET_SOURCE
 
-ECHO
-
+@ECHO ON
 git clone https://github.com/Epi-Info/Epi-Info-Community-Edition.git
-CD Epi-Info-Community-Edition
+@ECHO OFF
 
 COLOR 
-IF NOT %QUIET%==TRUE PAUSE
+:: IF NOT %QUIET%==TRUE PAUSE
 REM [COMMENT] Dashboard: Added paired t-test results to HTML output
 REM LAST CHECK-IN BEFORE CHANGE TO .NET 4.6.1 
 ::git reset --hard b414fa8dac477e93c1c0dad2b4080425044657d5
@@ -79,37 +81,61 @@ REM LAST CHECK-IN BEFORE CHANGE TO .NET 4.6.1
 REM [COMMENT] [BUILD] 7.2.2.16 11/2/2018
 ::git reset --hard 3d7b050cbde62137f77d9ffc4175a0ae8c409935
 
-:: ===============================================================
 :SKIP_GET_SOURCE
 :: ===============================================================
-
-:: ===============================================================
-:: UPDATE VERSION
+:: END OF GET SOURCE - GIT CLONE EPI INFO REPO
 :: ===============================================================
 
-:: SolutionInfo.cs
-:: [assembly: AssemblyVersion("7.2.*")]
-:: [assembly: AssemblyFileVersion("7.2.2.16")]
-:: [assembly: AssemblyInformationalVersion("7.2.2.16")]
-:: [assembly: SatelliteContractVersion("7.0.0.0")]
-:: [assembly: Epi.AssemblyReleaseDateAttribute("11/02/2018")]
+@ECHO ON
+CD Epi-Info-Community-Edition
+@ECHO OFF
 
-:: AssemblyInfo.cs
-:: [assembly: AssemblyVersion("7.2.2.16")]
-:: [assembly: AssemblyFileVersion("7.2.2.16")]
-
-:: AssemblyInfo.vb
-:: <Assembly: AssemblyVersion("7.2.2.16")>
-:: <Assembly: AssemblyFileVersion("7.2.2.16")>
 IF %QUIET%==TRUE GOTO:SKIP_UPDATE_VERSION
+ECHO :: ===============================================================
+ECHO :: UPDATE VERSION
+ECHO :: ===============================================================
+ECHO .
+ECHO :: SolutionInfo.cs
+ECHO :: [assembly: AssemblyVersion("7.2.*")]
+ECHO :: [assembly: AssemblyFileVersion("7.2.2.V")]
+ECHO :: [assembly: AssemblyInformationalVersion("7.2.2.V")]
+ECHO :: [assembly: SatelliteContractVersion("7.0.0.0")]
+ECHO :: [assembly: Epi.AssemblyReleaseDateAttribute("MM/DD/YYYY")]
+ECHO .
+ECHO :: AssemblyInfo.cs
+ECHO :: [assembly: AssemblyVersion("7.2.2.V")]
+ECHO :: [assembly: AssemblyFileVersion("7.2.2.V")]
+ECHO .
+ECHO :: AssemblyInfo.vb
+ECHO :: Assembly: AssemblyVersion("7.2.2.V")
+ECHO :: Assembly: AssemblyFileVersion("7.2.2.V")
+ECHO .
+ECHO An instance of Visual Studio Code will open and you can make the changes there.
+ECHO .
+PAUSE
 CALL code -n SolutionInfo.cs .\EpiInfoPlugin\Properties\AssemblyInfo.cs ".\StatisticsRepository\My Project\AssemblyInfo.vb"
+
+SET /P commit=COMMIT CHANGES TO GITHUB [Y/N]?
+:ASK_COMMIT_CHANGES
+IF /I "%commit%" EQU "Y" GOTO :COMMIT_CHANGES
+IF /I "%commit%" EQU "N" GOTO :SKIP_COMMIT_CHANGES
+GOTO :ASK_COMMIT_CHANGES
+GOTO :SKIP_COMMIT_CHANGES
+:COMMIT_CHANGES
+CHDIR
+START /WAIT ..\epi-info-git-commit.bat
+PAUSE
+
+:SKIP_COMMIT_CHANGES
 :SKIP_UPDATE_VERSION
 :: ===============================================================
+:: END OF UPDATE VERSION
+:: ===============================================================
 
 
-:: ===============================================================
-:: COPY KEYS
-:: ===============================================================
+ECHO :: ===============================================================
+ECHO :: COPY KEYS
+ECHO :: ===============================================================
 IF %QUIET%==TRUE GOTO:SKIP_KEYS_COPY
 :: ---------------------------------------------------------------
 :: CHANGE TO RELEASE KEYS
@@ -119,9 +145,10 @@ COPY /Y "C:\requiredFiles (ei7)\Configuration_Static.cs" .\Epi.Core\Configuratio
 ::ECHO OPEN IN CODE TO VERIFY ONLY KEYS HAVE CHANGED
 ::CALL code -n .\Epi.Core\Configuration_Static.cs
 ::PAUSE
-:: ---------------------------------------------------------------
-:: REPLACE THE COMPONENT ART LICENSE
-:: ---------------------------------------------------------------
+
+ECHO :: ===============================================================
+ECHO :: REPLACE THE COMPONENT ART LICENSE
+ECHO :: ===============================================================
 COPY /Y "C:\requiredFiles (ei7)\ComponentArt.Win.DataVisualization.lic" .\Epi.Windows.AnalysisDashboard\ComponentArt.Win.DataVisualization.lic
 COPY /Y "C:\requiredFiles (ei7)\ComponentArt.Win.DataVisualization.lic" .\Epi.Windows.Enter\ComponentArt.Win.DataVisualization.lic
 COPY /Y "C:\requiredFiles (ei7)\ComponentArt.Win.DataVisualization.lic" .\EpiDashboard\ComponentArt.Win.DataVisualization.lic
@@ -132,9 +159,9 @@ PAUSE
 :: ===============================================================
 
 
-:: ===============================================================
-:: COPY DLLS
-:: ===============================================================
+ECHO :: ===============================================================
+ECHO :: COPY DLLS
+ECHO :: ===============================================================
 IF NOT EXIST "build" (
     COLOR
     MKDIR build
@@ -152,9 +179,9 @@ COPY /Y "C:\requiredFiles (ei7)\dll\Interop.PortableDeviceTypesLib.dll" .\build\
 COPY /Y "C:\requiredFiles (ei7)\dll\Mono.Security.dll" .\build\release\Mono.Security.dll
 COPY /Y "C:\requiredFiles (ei7)\dll\Npgsql.dll" .\build\release\Npgsql.dll
 
-:: ===============================================================
-:: COPY PROJECT (RELEASE) DIRECTORY
-:: ===============================================================
+ECHO :: ===============================================================
+ECHO :: COPY PROJECT (RELEASE) DIRECTORY
+ECHO :: ===============================================================
 
 IF NOT %QUIET%==TRUE PAUSE
 
