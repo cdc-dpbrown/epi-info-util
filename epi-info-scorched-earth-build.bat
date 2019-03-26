8e8d03d52f3754da9f646ad6ba10a0ddd6ad1e1b
cdc-dpbrown <18171996+cdc-dpbrown@users.noreply.github.com>
2019-03-26 11:18:38 -0400

    ...

--- epi-info-scorched-earth-build.bat
+++ epi-info-scorched-earth-build.bat
@@ -34,18 +34,14 @@ SET /P d=DELETE EPI INFO FOLDER [Y/N]?
 IF /I "%d%" EQU "Y" GOTO :DELETEEPIINFOFOLDER
 IF /I "%d%" EQU "N" GOTO :SKIP_DELETE
 GOTO :ASK_SKIP_DELETE
-GOTO :SKIP_DELETE
 :DELETEEPIINFOFOLDER
-
 IF EXIST ".\Epi-Info-Community-Edition" (
     ECHO.
     ECHO Deleting Epi-Info-Community-Edition directory
 ) 
-
 IF EXIST ".\Epi-Info-Community-Edition" (
     RMDIR /S /Q .\Epi-Info-Community-Edition
 ) 
-
 IF EXIST ".\Epi-Info-Community-Edition" (
     COLOR 0A
     ECHO Epi-Info-Community-Edition - still there - try restartExplorer.bat
@@ -54,35 +50,25 @@ IF EXIST ".\Epi-Info-Community-Edition" (
     COLOR
     ECHO Epi-Info-Community-Edition - gone
 )
-
 :SKIP_DELETE
-
 ECHO :: ===============================================================
 ECHO :: GET SOURCE - GIT CLONE EPI INFO REPO
 ECHO :: ===============================================================
 IF %QUIET%==TRUE GOTO:GET_SOURCE
+IF /I "%d%" EQU "Y" GOTO :GET_SOURCE
 :ASK_GET_SOURCE
 SET /P o=OVERWRITE (GET) EPI INFO FROM GITHUB [Y/N]?
 IF /I "%o%" EQU "Y" GOTO :GET_SOURCE
 IF /I "%o%" EQU "N" GOTO :SKIP_GET_SOURCE
 GOTO :ASK_GET_SOURCE
-GOTO :SKIP_GET_SOURCE
 :GET_SOURCE
-
 @ECHO ON
 git clone https://github.com/Epi-Info/Epi-Info-Community-Edition.git
 @ECHO OFF
-
-COLOR 
-
 REM [COMMENT] [BUILD] 7.2.2.16 11/2/2018
 ::git reset --hard 3d7b050cbde62137f77d9ffc4175a0ae8c409935
-
 :SKIP_GET_SOURCE
-
-@ECHO ON
-CD %ei7%
-@ECHO OFF
+:: ===============================================================
 
 IF %QUIET%==TRUE GOTO:SKIP_UPDATE_VERSION
 ECHO :: ===============================================================
@@ -108,7 +94,6 @@ ECHO An instance of Visual Studio Code will open and you can make the changes th
 ECHO .
 PAUSE
 CALL code -n %ei7%\SolutionInfo.cs %ei7%\EpiInfoPlugin\Properties\AssemblyInfo.cs %ei7%"\StatisticsRepository\My Project\AssemblyInfo.vb"
-
 SET /P commit=COMMIT CHANGES TO GITHUB [Y/N]?
 :ASK_COMMIT_CHANGES
 IF /I "%commit%" EQU "Y" GOTO :COMMIT_CHANGES
@@ -117,9 +102,6 @@ GOTO :ASK_COMMIT_CHANGES
 GOTO :SKIP_COMMIT_CHANGES
 :COMMIT_CHANGES
 CHDIR
-:: ===============================================================
-
-:: START /WAIT %initialDirectory%\epi-info-git-commit.bat
 ECHO :: ===============================================================
 ECHO :: UPDATE VERSION
 ECHO :: [BUILD] 7.2.2.V M/D/20YY 
@@ -132,9 +114,8 @@ SET /P Y=Year(YY):
 SET commit_message=[BUILD] 7.2.2.%V% %M%/%D%/20%Y%
 ECHO %commit_message%
 ECHO .
-
-CD Epi-Info-Community-Edition
 @ECHO ON
+CD %ei7%
 git status
 git add SolutionInfo.cs .\EpiInfoPlugin\Properties\AssemblyInfo.cs ".\StatisticsRepository\My Project\AssemblyInfo.vb"
 git status
@@ -195,8 +176,6 @@ IF NOT EXIST ".\build\release" (
 ECHO :: ===============================================================
 ECHO :: OPEN VS 2017 AND BUILD
 ECHO :: ===============================================================
-:: https://docs.microsoft.com/en-us/visualstudio/ide/reference/devenv-command-line-switches?view=vs-2017
-:: devenv mysln.sln /build Release /project proj1 /projectconfig Release
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" %ei7%"\Epi Info 7.sln" /p:Configuration=Release /p:Platform=x86
 IF NOT %QUIET%==TRUE PAUSE
 :: ===============================================================
