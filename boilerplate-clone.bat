1ec38ea57b112183a1df854427142b35d05cd93a
cdc-dpbrown <18171996+cdc-dpbrown@users.noreply.github.com>
2019-05-07 09:11:13 -0400

    Run boilerplate

--- boilerplate-clone.bat
+++ boilerplate-clone.bat
@@ -22,25 +22,12 @@ IF EXIST ".\epi-info-boilerplate" (
 ECHO ON
 COLOR 
 
-REM git clone --depth 1 --single-branch --branch master https://github.com/cdc-dpbrown/epi-info-boilerplate.git epi-info-boilerplate
 git clone https://github.com/cdc-dpbrown/epi-info-boilerplate.git epi-info-boilerplate
-cd epi-info-boilerplate
+CD epi-info-boilerplate
 
-COLOR 
+dotnet restore
+dotnet build
 
-REM [comment]
-::git reset --hard 5bac8b278467a63d6c085387b8ae8cb7e36563c5
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:5000/
 
-REM [comment] 
-::git reset --hard 3c632651022ffe4f43f2b1b4c12a9a5cc2c7e828
-
-IF EXIST ".\package-lock.json" (
-    ECHO.
-    ECHO Deleting package-lock.json
-    DEL /Q .\package-lock.json
-) 
-
-CALL yarn
-:: yarn list --pattern "epi-"
-
-CALL yarn start
+dotnet run
