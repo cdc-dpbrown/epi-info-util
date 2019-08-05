20add02c7ded540e07863304cae149cc71e5c544
cdc-dpbrown <18171996+cdc-dpbrown@users.noreply.github.com>
2019-08-05 11:58:55 -0400

    Added timer.bat

---
+++ timer.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+:START
+:: CLS
+ECHO :: ===================================
+ECHO :: SEVEN POINT FIVE MINUTE TIMER
+ECHO :: ===================================
+COLOR 02
+TIMEOUT /T 450
+COLOR 0C
+PAUSE
+GOTO :START
