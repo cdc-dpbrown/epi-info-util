68f5e955b5193766fd0a3a9f52b68cd0b6eda367
cdc-dpbrown <ita3@cdc.gov>
2020-06-22 11:11:49 -0400

    Added Web Enter merge script.

--- web-enter-scorched-earth-merge.bat
+++ web-enter-scorched-earth-merge.bat
@@ -2,6 +2,7 @@ CLS
 @ECHO OFF
 COLOR 0A
 
+
 ECHO :: ===============================================================
 ECHO :: SET LOCAL VARIABLES
 ECHO :: ===============================================================
@@ -9,7 +10,8 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 SET buildEXE="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe"
 SET batchRootDirectory=%CD%
 SET privateBuildFiles=C:\epi-info-private-build-files
-SET webPath=%batchRootDirectory%\Epi-Info-Web
+SET webEnterMergePath=%batchRootDirectory%\Epi-Info-Web-Enter-Merge
+SET webSurvey=%batchRootDirectory%\Epi-Info-Web-Survey
 SET KEY_QUIET=Q
 SET KEY_HELP=/?
 SET ARGS=%1
@@ -22,11 +24,12 @@ ECHO ARGS=%ARGS%
 ECHO QUIET: %QUIET%
 ECHO HELP: %HELP%
 ECHO batchRootDirectory: %batchRootDirectory%
-ECHO webPath: %webPath%
-:: ===============================================================
+ECHO webEnterMergePath:  %webEnterMergePath%
+ECHO webSurvey: %webSurvey%
+
 
 ECHO :: ===============================================================
-ECHO :: DELETE WEB SURVEY FOLDER
+ECHO :: DELETE WEB ENTER MERGE FOLDER
 ECHO :: ===============================================================
 IF %QUIET%==TRUE GOTO :DELETEEPIINFOFOLDER
 :ASK_SKIP_DELETE
@@ -36,48 +39,67 @@ IF /I "%d%" EQU "N" GOTO :SKIP_DELETE
 GOTO :ASK_SKIP_DELETE
 :DELETEEPIINFOFOLDER
 
-IF EXIST ".\Epi-Info-Web" (
+IF EXIST  %webEnterMergePath% (
     ECHO.
-    ECHO Deleting Epi-Info-Web directory
+    ECHO Deleting %webEnterMergePath% directory
 ) 
 
-IF EXIST ".\Epi-Info-Web" (
-    RMDIR /S /Q .\Epi-Info-Web
+IF EXIST  %webEnterMergePath% (
+    RMDIR /S /Q  %webEnterMergePath%
 )
 
-IF EXIST ".\Epi-Info-Web" (
+IF EXIST  %webEnterMergePath% (
     COLOR 0A
-    ECHO Epi-Info-Web - still there - try restartExplorer.bat
+    ECHO  %webEnterMergePath% - still there - try restartExplorer.bat
     EXIT /B
 ) ELSE (
     COLOR
-    ECHO Epi-Info-Web - gone
+    ECHO  %webEnterMergePath% - gone
 )
 :SKIP_DELETE
+
+
 ECHO :: ===============================================================
-ECHO :: GET SOURCE - GIT CLONE WEB REPO
+ECHO :: GET SOURCE - GIT CLONE WEB ENTER REPO INTO WEB ENTER MERGE
 ECHO :: ===============================================================
 IF %QUIET%==TRUE GOTO:GET_SOURCE
 IF /I "%d%" EQU "Y" GOTO :GET_SOURCE
 :ASK_GET_SOURCE
-SET /P o=OVERWRITE (GET) WEB SURVEY FROM GITHUB [Y/N]?
+SET /P o=OVERWRITE (GET) FROM GITHUB [Y/N]?
 IF /I "%o%" EQU "Y" GOTO :GET_SOURCE
 IF /I "%o%" EQU "N" GOTO :SKIP_GET_SOURCE
 GOTO :ASK_GET_SOURCE
 :GET_SOURCE
 @ECHO ON
-git clone https://github.com/cdc-dpbrown/epi-info-web.git
+git clone https://github.com/Epi-Info/Epi-Info-Cloud-Data-Capture.git
 @ECHO OFF
-::CD %webPath%
+::CD  webEnterMergePath%
 ::git reset --hard c5baca6a6c08f2168bc28d00f7db0e2ce104fd24
 :SKIP_GET_SOURCE
-:: ===============================================================
+
+
+
+ECHO :: ===============================================================
+ECHO :: COPY WEB SURVEY FILES TO WEB ENTER FOLDER
+ECHO :: ===============================================================
+@ECHO ON
+XCOPY %webSurvey% %webEnterMergePath% /I /E /Q
+@ECHO OFF
+
+GIT DIFF
+
+
+
+
+
+
+
 
 ECHO :: ===============================================================
 ECHO :: OPEN WINDOWS EXPLORER IN WEB SURVEY DIRECTORY
 ECHO :: ===============================================================
 @ECHO ON
-::EXPLORER %webPath%
+::EXPLORER  webEnterMergePath%
 @ECHO OFF
 :: ===============================================================
 
@@ -87,8 +109,19 @@ ECHO :: COPY WEB CONFIG
 ECHO :: ===============================================================
 ECHO.
 @ECHO ON
-COPY /Y %privateBuildFiles%\cloud-data-capture\Web.config %webPath%\Epi.Web\Web.config
+COPY /Y %privateBuildFiles%\cloud-data-capture\Web.config  webEnterMergePath%\Epi.Web\Web.config
 @ECHO OFF
+
+
+
+
+
+GOTO :EOF
+
+
+
+
+
 
 ECHO :: ===============================================================
 ECHO :: OPEN SOLUTION IN VISUAL STUDIO
@@ -96,14 +129,14 @@ ECHO :: ===============================================================
 ECHO.
 @ECHO ON
 CD %batchRootDirectory%
-CALL nuget restore %webPath%"\Epi Info Web.sln"
+CALL nuget restore  webEnterMergePath%"\Epi Info Web.sln"
 @ECHO OFF
 ECHO.
 ECHO.
 ECHO     [ STARTING BUILD ] PLEASE WAIT ... ( ~30 SECONDS )
 ECHO.
 ECHO.
-CD %webPath%
+CD  webEnterMergePath%
 :: CALL %buildEXE% "Epi Info Web.sln" /m /p:Configuration=Release /p:Platform=x86 /clp:Summary=true;ErrorsOnly
 @ECHO OFF
 :: ===============================================================
@@ -112,7 +145,7 @@ ECHO :: ===============================================================
 ECHO :: OPEN SOLUTION IN VISUAL STUDIO
 ECHO :: ===============================================================
 @ECHO ON
-CD %webPath%
+CD  webEnterMergePath%
 CALL "Epi Info Web.sln"
 @ECHO OFF
 ECHO :: ===============================================================
